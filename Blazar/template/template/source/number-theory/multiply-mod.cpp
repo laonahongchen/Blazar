5b6f37d5476e56a9ed0404c61bc44cafa68abe38
laonahongchen <chenweizhe1999@126.com>
2017-10-15 20:58:33 +0800

    formate

--- Blazar/template/template/source/number-theory/multiply-mod.cpp
+++ Blazar/template/template/source/number-theory/multiply-mod.cpp
@@ -1,14 +1,14 @@
-LL mul(LL a,LL b,LL p){//保证a,b非负 
-    a=(a+p)%p; 
-    b=(b+p)%p;
-	LL t=(a*b-LL((long double)a*b/p)*p)%p;
-	return (t+p)%p;	
+LL mul(LL a, LL b, LL p) { //保证a,b非负
+    a = (a + p) % p;
+    b = (b + p) % p;
+    LL t = (a * b - LL((long double)a * b / p) * p) % p;
+    return (t + p) % p;
 }
-LL mul(LL x,LL k,LL p){
-    LL ans=0;
-    for(;k;k>>=1){
-        if(k&1)ans=(ans+x)%p;
-        x=(x+x)%p;
+LL mul(LL x, LL k, LL p) {
+    LL ans = 0;
+    for(; k; k >>= 1) {
+        if(k & 1) ans = (ans + x) % p;
+        x = (x + x) % p;
     }
     return ans;
 }
