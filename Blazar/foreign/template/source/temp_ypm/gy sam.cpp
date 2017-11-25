4faab94fd7989fdd410f6e67deb57d4aace65c87
laonahongchen <chenweizhe1999@126.com>
2017-11-25 19:03:34 +0800

    ypm added

--- Blazar/foreign/template/source/temp_ypm/gy sam.cpp
+++ Blazar/foreign/template/source/temp_ypm/gy sam.cpp
@@ -1,57 +1,39 @@
-const int N = 251010;
-const int C = 26;
-
+const int N = 251010, C = 26;
 int tot, las, root;
 struct Node
 {
 	int son[C], len, par;
-	void clear()
-	{
+	void clear(){
 		memset(son, 0, sizeof(son));
 		par = len = 0;
 	}
 }node[N << 1];
-
-inline int newNode()
-{
-	node[++tot].clear();
-	return tot;
-}
-
+inline int newNode(){return node[++tot].clear(), tot;}
 void extend(int c) 
 {
 	int p = las;
 	if (node[p].son[c]) {
 		int q = node[p].son[c];
-		if (node[p].len + 1 == node[q].len) 
-			las = q;
-		else
-		{
+		if (node[p].len + 1 == node[q].len)	las = q;
+		else{
 			int nq = newNode(); 
-			las = nq;
-			node[nq] = node[q];
-			node[nq].len = node[p].len + 1;
-			node[q].par = nq;
+			las = nq; node[nq] = node[q];
+			node[nq].len = node[p].len + 1;	node[q].par = nq;
 			for (; p && node[p].son[c] == q; p = node[p].par)
 				node[p].son[c] = nq;
 		}
 	}
-	else // Naive Suffix Automaton
-	{
+	else{ // Naive Suffix Automaton
 		int np = newNode();
-		las = np;
-		node[np].len = node[p].len + 1;
+		las = np; node[np].len = node[p].len + 1;
 		for (; p && !node[p].son[c]; p = node[p].par)
 			node[p].son[c] = np;
-		if (!p) 
-			node[np].par = root;
-		else
-		{
+		if (!p) node[np].par = root;
+		else{
 			int q = node[p].son[c];
 			if (node[p].len + 1 == node[q].len)
 				node[np].par = q;
-			else
-			{
+			else{
 				int nq = newNode();
 				node[nq] = node[q];
 				node[nq].len = node[p].len + 1;
@@ -62,12 +44,8 @@ void extend(int c)
 		}
 	}
 }
-
 void add(char *s)
 {
-	int len = strlen(s + 1);
-	las = root;
-	for(int i = 1; i <= len; i++)
-		extend(s[i] - 'a');
+	int len = strlen(s + 1); las = root;
+	for(int i = 1; i <= len; i++) extend(s[i] - 'a');
 }
-
--- Blazar/foreign/template/source/temp_ypm/manacher-poj3974.cpp
+++ Blazar/foreign/template/source/temp_ypm/manacher-poj3974.cpp
@@ -1,30 +1,15 @@
-int manacher(char *st)
-{
-	const int N = 1e6+10;
-	static char s[N << 1];
+void manacher(char *st){
+	static char s[N << 1]; 
 	static int p[N << 1];
 	int n = strlen(st + 1);
-	s[0] = '$';
-	s[1] = '#';
+	s[0] = '$'; s[1] = '#';
 	for(int i = 1; i <= n; i++)
-	{
-		s[i << 1] = st[i];
-		s[(i << 1) + 1] = '#';
-	}
-	n = n * 2 + 1;
-	s[n + 1] = 0;
+		s[i << 1] = st[i], s[(i << 1) + 1] = '#';
+	s[(n = n * 2 + 1) + 1] = 0;
 	int pos, mx = 0, res = 0;
-	for(int i = 1; i <= n; i++)
-	{
+	for(int i = 1; i <= n; i++){
 		p[i] = (mx > i) ? min(p[pos * 2 - i], mx - i) : 1;
-		while(s[i + p[i]] == s[i - p[i]])
-			p[i]++;
-		if(p[i] + i - 1 > mx)
-		{
-			mx = p[i] + i - 1;
-			pos = i;
-		}
-		res = max(p[i], res);
+		while(s[i + p[i]] == s[i - p[i]]) p[i]++;
+		if(p[i] + i - 1 > mx) mx = p[i] + i - 1, pos = i;
 	}
-	return res - 1;
 }
